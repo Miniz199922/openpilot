d5fbc72ef811e4c9097c8d1ace5d73e329d77954
John <4vanetten@gmail.com>
2024-05-11 21:05:00 -0600

    only if enabled

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -143,7 +143,7 @@ class CarController:
 
     if CS.das_3["COUNTER"] != self.last_das_3_counter:
       self.last_das_3_counter = CS.das_3["COUNTER"]
-      if CS.out.vEgo < 2.:
+      if CC.enabled and CS.out.vEgo < 2.:
         stop = CC.jvePilotState.carControl.brakeHold
         can_sends.append(chryslercan.create_das_3_standstill(self.packer, CS.das_3, stop))
 
